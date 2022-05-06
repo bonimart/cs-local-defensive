bb317998187bbfe856db1823978d14528cdf5feb
Martin Benes <benesm44@fit.cvut.cz>
2022-05-06 20:59:31 +0200

    add configuration

---
+++ semestral/app/utils/config.py
@@ -0,0 +1,19 @@
+config = {
+    'player': {
+        'radius': 40,
+        'speed': 150,
+        'max_hp': 100,
+        'self_color': (0, 255, 255),
+        'ally_color': (0, 255, 0),
+        'enemy_color': (255, 0, 0)
+    },
+    'bullet': {
+        'radius': 10,
+        'speed': 10
+    },
+    'window': {
+        'height': 800,
+        'width': 800
+    },
+    'frame_rate': 120
+}
