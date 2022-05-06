87ec9ad0dee660c266209cb19ae1d7778002f05f
Martin Benes <benesm44@fit.cvut.cz>
2022-05-06 21:03:46 +0200

    add Bullet class

---
+++ semestral/app/src/objects/Bullet.py
@@ -0,0 +1,13 @@
+from src.objects.Movable import Movable
+from utils.Vector import Vector
+
+
+class Bullet(Movable):
+    def __init__(self, x, y):
+        pass
+
+    def move(self):
+        pass
+
+    def hit():
+        pass
