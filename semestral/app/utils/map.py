ce546fc24413ff4dfbf6feef5d0cf27ba5efccff
Martin Benes <benesm44@fit.cvut.cz>
2022-05-07 16:34:57 +0200

    add map, stores static objects, which are used only for collisions

---
+++ semestral/app/utils/map.py
@@ -0,0 +1,20 @@
+from utils.config import config
+
+"""
+list of all static objects in the game in format (center_x, center_y, width, height)
+"""
+mp = [(410, 200, 220, config['player']['radius']),
+      (880, 200, 370, config['player']['radius']),
+      (350, 450, 300, config['player']['radius']),
+      (850, 450, 300, config['player']['radius']),
+      (200, 600, 300, config['player']['radius']),
+      (900, 600, 200, config['player']['radius']),
+      (1300, 600, 200, config['player']['radius']),
+      (1000, 700, config['player']['radius'], 220),
+      (1000, 385, config['player']['radius'], 150),
+      (1220, 270, config['player']['radius'], 350),
+      (510, 500, config['player']['radius'], 350),
+      (160, 200, config['player']['radius'], 250),
+      (710, 50, config['player']['radius'], 100),
+      (510, 500, config['player']['radius'], 350),
+      ]
