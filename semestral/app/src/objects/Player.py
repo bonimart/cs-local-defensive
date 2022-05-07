fe253f3a856cc9c6e74181c809fc0b7e8aa6847c
Martin Benes <benesm44@fit.cvut.cz>
2022-05-07 16:32:22 +0200

    rework player update method

--- semestral/app/src/objects/Player.py
+++ semestral/app/src/objects/Player.py
@@ -1,4 +1,4 @@
-from src.objects.Movable import Movable
+from src.objects.ObjectCircle import ObjectCircle
 from utils.Vector import Vector
 from utils.config import config
 from pyglet.window import key
@@ -6,17 +6,15 @@ import pyglet.shapes as sh
 from math import sin, cos, radians
 
 
-class Player(Movable, key.KeyStateHandler):
+class Player(ObjectCircle, key.KeyStateHandler):
     speed = config['player']['speed']
 
     def __init__(self, x, y, rotation, batch):
-        super().__init__(x, y)
+        super().__init__(x, y, config['player']['radius'],
+                         color=config['player']['self_color'], batch=batch)
         self.vel = Vector(0, 0)
         self.rotation = 0
         self.vel = Vector(0, 0)
-        self.shape = sh.Circle(
-            self.pos.x, self.pos.y, config['player']['radius'],
-            color=config['player']['self_color'], batch=batch)
 
     def update(self, dt):
         self.vel.y = self[key.UP] - self[key.DOWN]
@@ -24,8 +22,6 @@ class Player(Movable, key.KeyStateHandler):
         self.vel.normalize()
 
         self.pos += self.vel * Player.speed * dt
-        self.shape.anchor_x += self.vel.x * Player.speed * dt
-        self.shape.anchor_y += self.vel.y * Player.speed * dt
 
     def shoot(self):
         pass
