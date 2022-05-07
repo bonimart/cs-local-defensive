96fd35cacd8669b9a80c7b97c7a845f891631e5b
Martin Benes <benesm44@fit.cvut.cz>
2022-05-07 16:31:05 +0200

    rework game with collisions

--- semestral/app/src/objects/Game.py
+++ semestral/app/src/objects/Game.py
@@ -1,13 +1,17 @@
 from src.objects.Player import Player
+from src.objects.ObjectRect import ObjectRect
+from src.objects.ObjectCircle import ObjectCircle
 from utils.config import config
+from utils.map import mp
 import pyglet
 
 
 class Game:
-    def __init__(self):
-        self.players = set()
+    def __init__(self, win):
+        self.players = []
         self.bullets = set()
         self.objects = set()
+        self.win = win
 
     def isOver(self):
         t_dead = True
@@ -23,15 +27,40 @@ class Game:
         for p in self.players:
             p.update(dt)
 
+            if p.pos.x - p.r < 0:
+                p.pos.x = p.r
+            elif p.pos.x + p.r > self.win.width:
+                p.pos.x = self.win.width - p.r
+
+            if p.pos.y - p.r < 0:
+                p.pos.y = p.r
+            elif p.pos.y + p.r > self.win.height:
+                p.pos.y = self.win.height - p.r
+
+            for o in self.objects:
+                if p.is_colliding(o):
+                    p.resolve_collision(o)
+
+            p.shape.x = p.pos.x
+            p.shape.y = p.pos.y
+
 
 def run_game():
     win = pyglet.window.Window(
         config['window']['width'], config['window']['height'])
     batch = pyglet.graphics.Batch()
 
+    g = Game(win)
+
+    #r = ObjectRect(120, 200, 100, 200, batch=batch)
     p = Player(win.width//2, win.width//2, 0, batch)
+    g.players.append(p)
+
+    for o in mp:
+        g.objects.add(ObjectRect(*o, batch=batch))
+
     win.push_handlers(p)
-    pyglet.clock.schedule_interval(p.update, 1 / 120.)
+    pyglet.clock.schedule_interval(g.update, 1 / config['frame_rate'])
 
     @win.event
     def on_draw():
