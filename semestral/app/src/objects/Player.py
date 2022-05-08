82f66153419b54d518bb343ec8f3e120ac359633
Martin Benes <benesm44@fit.cvut.cz>
2022-05-08 22:03:37 +0200

    add hit points and shoot method to Player

--- semestral/app/src/objects/Player.py
+++ semestral/app/src/objects/Player.py
@@ -1,30 +1,43 @@
 from src.objects.ObjectCircle import ObjectCircle
+from src.objects.Bullet import Bullet
 from utils.Vector import Vector
 from utils.config import config
-from pyglet.window import key
+from pyglet.window import key, mouse
 import pyglet.shapes as sh
 from math import sin, cos, radians
 
 
-class Player(ObjectCircle, key.KeyStateHandler):
+class Player(ObjectCircle):
     speed = config['player']['speed']
 
     def __init__(self, x, y, rotation, batch):
         super().__init__(x, y, config['player']['radius'],
-                         color=config['player']['self_color'], batch=batch)
+                         color=config['color']['self'], batch=batch)
         self.vel = Vector(0, 0)
+        self.hp = config['player']['max_hp']
         self.rotation = 0
         self.vel = Vector(0, 0)
+        self.key_handler = key.KeyStateHandler()
 
     def update(self, dt):
-        self.vel.y = self[key.UP] - self[key.DOWN]
-        self.vel.x = self[key.RIGHT] - self[key.LEFT]
-        self.vel.normalize()
+        """Updates player position and velocity based on user input
+
+        Args:
+            dt (float): difference in time
+        """
+        self.vel.y = self.key_handler[key.UP] - self.key_handler[key.DOWN]
+        self.vel.x = self.key_handler[key.RIGHT] - self.key_handler[key.LEFT]
+        self.vel = self.vel.normalize()
 
         self.pos += self.vel * Player.speed * dt
 
-    def shoot(self):
-        pass
+    def shoot(self, x, y):
+        v = Vector(x - self.pos.x, y - self.pos.y).normalize()
+        return Bullet(self.pos.x + v.x * (self.r + 2 * config['bullet']['radius']),
+                      self.pos.y + v.y *
+                      (self.r + 2 * config['bullet']['radius']),
+                      v.x, v.y,
+                      batch=self.shape._batch)
 
     def isHit():
         pass
