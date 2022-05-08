7e39cd3389c028a0b4a5fbf5a331e40325c2ca24
Martin Benes <benesm44@fit.cvut.cz>
2022-05-08 22:06:14 +0200

    add bullets and collisions with them

--- semestral/app/src/objects/Game.py
+++ semestral/app/src/objects/Game.py
@@ -7,8 +7,8 @@ import pyglet
 
 
 class Game:
-    def __init__(self, win):
-        self.players = []
+    def __init__(self, win, walls):
+        self.players = set()
         self.bullets = set()
         self.objects = set()
         self.win = win
@@ -23,26 +23,49 @@ class Game:
                 ct_dead = False
         return ct_dead or t_dead
 
+    def update_movable(self, obj, dt):
+        obj.update(dt)
+        for o in self.objects:
+            if obj.is_colliding(o):
+                obj.resolve_collision(o)
+
+        obj.shape.x = obj.pos.x
+        obj.shape.y = obj.pos.y
+
     def update(self, dt):
+        """Updates all game entities
+
+        Args:
+            dt (float): difference in time
+        """
+        hit = set()
+        dead = set()
+
         for p in self.players:
-            p.update(dt)
+            self.update_movable(p, dt)
+            for b in self.bullets:
+                if p.is_colliding(b):
+                    hit.add(b)
+                    p.hp -= config['bullet']['damage']
+            if p.hp <= 0:
+                dead.add(p)
 
-            if p.pos.x - p.r < 0:
-                p.pos.x = p.r
-            elif p.pos.x + p.r > self.win.width:
-                p.pos.x = self.win.width - p.r
+        for p in dead:
+            p.shape.color = (255, 0, 0)
 
-            if p.pos.y - p.r < 0:
-                p.pos.y = p.r
-            elif p.pos.y + p.r > self.win.height:
-                p.pos.y = self.win.height - p.r
+        for b in hit:
+            self.bullets.remove(b)
 
-            for o in self.objects:
-                if p.is_colliding(o):
-                    p.resolve_collision(o)
+        exploded = set()
+        for b in self.bullets:
+            self.update_movable(b, dt)
+            for b2 in self.bullets - {b}:
+                if b.is_colliding(b2):
+                    exploded.add(b)
+                    exploded.add(b2)
 
-            p.shape.x = p.pos.x
-            p.shape.y = p.pos.y
+        for b in exploded:
+            self.bullets.remove(b)
 
 
 def run_game():
@@ -50,21 +73,36 @@ def run_game():
         config['window']['width'], config['window']['height'])
     batch = pyglet.graphics.Batch()
 
-    g = Game(win)
+    g = Game(win, mp)
 
     #r = ObjectRect(120, 200, 100, 200, batch=batch)
+    r = pyglet.shapes.Rectangle(0, 0, win.width, win.height,
+                                color=config['color']['background'], batch=batch)
     p = Player(win.width//2, win.width//2, 0, batch)
-    g.players.append(p)
+    g.players.add(p)
 
     for o in mp:
-        g.objects.add(ObjectRect(*o, batch=batch))
+        g.objects.add(ObjectRect(
+            *o, color=config['color']['wall'], batch=batch))
 
-    win.push_handlers(p)
+    win.push_handlers(p.key_handler)
     pyglet.clock.schedule_interval(g.update, 1 / config['frame_rate'])
 
     @win.event
     def on_draw():
         win.clear()
         batch.draw()
+
+    @win.event
+    def on_mouse_press(x, y, button, modifiers):
+        b = p.shoot(x, y)
+        shot_himself = False
+        for o in g.objects:
+            if b.is_colliding(o):
+                shot_himself = True
+        if shot_himself:
+            p.hp -= config['bullet']['damage']
+        else:
+            g.bullets.add(b)
 
     pyglet.app.run()
