3f56cb8bff403e7dd2db686acebac295215b1af3
Martin Benes <benesm44@fit.cvut.cz>
2022-05-08 22:04:19 +0200

    coordinates in ObjectRect are now lower-left corner

--- semestral/app/src/objects/ObjectRect.py
+++ semestral/app/src/objects/ObjectRect.py
@@ -5,7 +5,7 @@ import pyglet.shapes as sh
 
 class ObjectRect(Object):
     def __init__(self, x, y, w, h, color=(255, 255, 255), batch=None):
-        super().__init__(x, y)
+        super().__init__(x+w//2, y+h//2)
         self.size = Vector(w, h)
         self.shape = sh.Rectangle(
-            x-w//2, y-h//2, w, h, color=color, batch=batch)
+            x, y, w, h, color=color, batch=batch)
