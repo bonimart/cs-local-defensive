20259688997f558137461b4920d6adfd26773afd
Martin Benes <benesm44@fit.cvut.cz>
2022-05-08 22:02:18 +0200

    tweak bullet and colors

--- semestral/app/utils/config.py
+++ semestral/app/utils/config.py
@@ -2,18 +2,25 @@ config = {
     'player': {
         'radius': 20,
         'speed': 200,
-        'max_hp': 100,
-        'self_color': (0, 255, 255),
-        'ally_color': (0, 255, 0),
-        'enemy_color': (255, 0, 0)
+        'max_hp': 100
     },
     'bullet': {
         'radius': 10,
-        'speed': 10
+        'speed': 500,
+        'damage': 20
     },
     'window': {
         'width': 1400,
-        'height': 800
+        'height': 800,
+        'border': 2
     },
-    'frame_rate': 120.
+    'frame_rate': 120.,
+    'color': {
+        'self': (235, 235, 235),
+        'ally': (42, 168, 118),
+        'enemy': (231, 76, 60),
+        'wall': (218, 150, 0),
+        'bullet': (131, 114, 53),
+        'background': (42, 36, 36)
+    }
 }
