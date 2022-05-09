c4557020e8c0cac1cf75bcb28b84eab2af15f0b5
Martin Benes <benesm44@fit.cvut.cz>
2022-05-09 11:02:26 +0200

    add settings for bot

--- semestral/app/utils/config.py
+++ semestral/app/utils/config.py
@@ -4,10 +4,14 @@ config = {
         'speed': 200,
         'max_hp': 100
     },
+    'bot': {
+        'search_radius': 500,
+        'kill_cd': 1
+    },
     'bullet': {
         'radius': 10,
         'speed': 500,
-        'damage': 20
+        'damage': 5
     },
     'window': {
         'width': 1400,
@@ -16,11 +20,11 @@ config = {
     },
     'frame_rate': 120.,
     'color': {
-        'self': (235, 235, 235),
+        'self': (55, 175, 195),
         'ally': (42, 168, 118),
         'enemy': (231, 76, 60),
         'wall': (218, 150, 0),
-        'bullet': (131, 114, 53),
+        'bullet': (131, 114, 93),
         'background': (42, 36, 36)
     }
 }
