f5c63b61d6ca8aed20aed98b519f64317e35eb0e
Martin Benes <benesm44@fit.cvut.cz>
2022-05-11 23:58:23 +0200

    add server config variables

--- semestral/app/utils/config.py
+++ semestral/app/utils/config.py
@@ -5,26 +5,40 @@ config = {
         'max_hp': 100
     },
     'bot': {
-        'search_radius': 500,
-        'kill_cd': 1
+        'search_radius': 300,
+        'kill_cd': .25,
+        'attraction_coefficient': 1/10000
+    },
+    'gun': {
+        'width': 10,
+        'length': 20
     },
     'bullet': {
         'radius': 10,
         'speed': 500,
-        'damage': 5
+        'damage': 20
     },
     'window': {
         'width': 1400,
         'height': 800,
         'border': 2
     },
-    'frame_rate': 120.,
     'color': {
         'self': (55, 175, 195),
         'ally': (42, 168, 118),
         'enemy': (231, 76, 60),
         'wall': (218, 150, 0),
         'bullet': (131, 114, 93),
+        'gun': (85, 85, 85),
         'background': (42, 36, 36)
-    }
+    },
+    'server': {
+        'ip': '192.168.1.19',
+        'port': 5555
+    },
+    'friendly_fire': False,
+    'frame_rate': 120.,
+    'update_rate': 120.,
+    'debug': False,
+    'num_of_players': 20
 }
