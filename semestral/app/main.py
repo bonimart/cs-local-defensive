3ae542f985f426ed01ba9f32b26e497d4017122b
Martin Benes <benesm44@fit.cvut.cz>
2022-05-11 23:57:21 +0200

    add logic to run client and server from main

--- semestral/app/main.py
+++ semestral/app/main.py
@@ -1,3 +1,13 @@
 from src.objects.Game import run_game
+from src.server.server import run_server
+from src.client.client import run_client
+from utils.config import config
+import sys
 
-run_game()
+if len(sys.argv) > 1:
+    if sys.argv[1] == "server":
+        run_server(config['server']['ip'], config['server']['port'])
+    elif sys.argv[1] == "client":
+        run_client(config['server']['ip'], config['server']['port'])
+else:
+    run_game()
