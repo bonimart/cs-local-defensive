4c0744c1a18d547b9bf459e24c910aed458e9e6b
Martin Benes <benesm44@fit.cvut.cz>
2022-05-14 22:56:52 +0200

    add help string and argument manipulation

--- semestral/app/main.py
+++ semestral/app/main.py
@@ -1,13 +1,37 @@
-from src.objects.Game import run_game
 from src.server.server import run_server
 from src.client.client import run_client
 from utils.config import config
 import sys
 
-if len(sys.argv) > 1:
-    if sys.argv[1] == "server":
-        run_server(config['server']['ip'], config['server']['port'])
-    elif sys.argv[1] == "client":
-        run_client(config['server']['ip'], config['server']['port'])
-else:
-    run_game()
+help_string = '''Usage: python3 main.py server|client [IP ADRESS]
+------------------------------------------------
+In order to play this 2D shooter, you need to connect to a local server.
+You can either start a new server and then connect to it, or connect to an existing server straight away.
+If you want to create a new server, use 'server' option, if you want to connect to a server, use 'client' option.
+IP ADRESS is optional, default ip and port are specified in /utils/config.py.'''
+
+if __name__ == "__main__":
+    n_arguments = len(sys.argv)
+
+    wrong_input = False
+
+    if n_arguments <= 1:
+        wrong_input = True
+
+    elif n_arguments > 1:
+        ip = config['server']['ip']
+        port = config['server']['port']
+        if n_arguments == 3:
+            ip = sys.argv[2]
+        elif n_arguments > 3:
+            wrong_input = True
+        if sys.argv[1] == "server":
+            run_server(ip, port)
+        elif sys.argv[1] == "client":
+            run_client(ip, port)
+        else:
+            wrong_input = True
+
+    if wrong_input:
+        print("Error: wrong arguments given\n")
+        print(help_string)
